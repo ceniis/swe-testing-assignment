5773ddee1a4771972bf3c3328aa70cecdd87f049
ceniis <ahtyrskaasofia@gmail.com>
2026-02-28 13:29:00 +0200

    Add UI and program logic

--- Quick-Calc/Form1.Designer.cs
+++ Quick-Calc/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Quick_Calc
 {
-    partial class Form1
+    partial class Form
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,356 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button0 = new Button();
+            panel1 = new Panel();
+            buttonEqual = new Button();
+            buttonC = new Button();
+            buttonDiv = new Button();
+            buttonSub = new Button();
+            buttonMul = new Button();
+            buttonAdd = new Button();
+            textDisplay2 = new TextBox();
+            textDisplay1 = new TextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightBlue;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            button1.ForeColor = Color.DarkBlue;
+            button1.Location = new Point(13, 13);
+            button1.Name = "button1";
+            button1.Size = new Size(40, 30);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += BtnNum_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.LightBlue;
+            button2.Cursor = Cursors.Hand;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            button2.ForeColor = Color.DarkBlue;
+            button2.Location = new Point(61, 13);
+            button2.Name = "button2";
+            button2.Size = new Size(40, 30);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += BtnNum_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.LightBlue;
+            button3.Cursor = Cursors.Hand;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            button3.ForeColor = Color.DarkBlue;
+            button3.Location = new Point(107, 13);
+            button3.Name = "button3";
+            button3.Size = new Size(40, 30);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += BtnNum_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.LightBlue;
+            button4.Cursor = Cursors.Hand;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            button4.ForeColor = Color.DarkBlue;
+            button4.Location = new Point(13, 49);
+            button4.Name = "button4";
+            button4.Size = new Size(40, 30);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += BtnNum_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.LightBlue;
+            button5.Cursor = Cursors.Hand;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            button5.ForeColor = Color.DarkBlue;
+            button5.Location = new Point(61, 49);
+            button5.Name = "button5";
+            button5.Size = new Size(40, 30);
+            button5.TabIndex = 5;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += BtnNum_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.LightBlue;
+            button6.Cursor = Cursors.Hand;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            button6.ForeColor = Color.DarkBlue;
+            button6.Location = new Point(107, 49);
+            button6.Name = "button6";
+            button6.Size = new Size(40, 30);
+            button6.TabIndex = 6;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += BtnNum_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.LightBlue;
+            button7.Cursor = Cursors.Hand;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            button7.ForeColor = Color.DarkBlue;
+            button7.Location = new Point(13, 85);
+            button7.Name = "button7";
+            button7.Size = new Size(40, 30);
+            button7.TabIndex = 7;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += BtnNum_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.LightBlue;
+            button8.Cursor = Cursors.Hand;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            button8.ForeColor = Color.DarkBlue;
+            button8.Location = new Point(61, 85);
+            button8.Name = "button8";
+            button8.Size = new Size(40, 30);
+            button8.TabIndex = 8;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += BtnNum_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.LightBlue;
+            button9.Cursor = Cursors.Hand;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            button9.ForeColor = Color.DarkBlue;
+            button9.Location = new Point(107, 85);
+            button9.Name = "button9";
+            button9.Size = new Size(40, 30);
+            button9.TabIndex = 8;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += BtnNum_Click;
+            // 
+            // button0
+            // 
+            button0.BackColor = Color.LightBlue;
+            button0.Cursor = Cursors.Hand;
+            button0.FlatStyle = FlatStyle.Flat;
+            button0.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            button0.ForeColor = Color.DarkBlue;
+            button0.Location = new Point(13, 121);
+            button0.Name = "button0";
+            button0.Size = new Size(88, 30);
+            button0.TabIndex = 9;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = false;
+            button0.Click += BtnNum_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.SteelBlue;
+            panel1.Controls.Add(buttonEqual);
+            panel1.Controls.Add(buttonC);
+            panel1.Controls.Add(buttonDiv);
+            panel1.Controls.Add(buttonSub);
+            panel1.Controls.Add(buttonMul);
+            panel1.Controls.Add(buttonAdd);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(button0);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button9);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button8);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button6);
+            panel1.Dock = DockStyle.Bottom;
+            panel1.Location = new Point(0, 73);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(205, 196);
+            panel1.TabIndex = 10;
+            // 
+            // buttonEqual
+            // 
+            buttonEqual.BackColor = Color.LightCyan;
+            buttonEqual.Cursor = Cursors.Hand;
+            buttonEqual.FlatStyle = FlatStyle.Flat;
+            buttonEqual.Font = new Font("Yu Gothic UI", 10F, FontStyle.Bold);
+            buttonEqual.ForeColor = Color.DarkBlue;
+            buttonEqual.Location = new Point(13, 157);
+            buttonEqual.Name = "buttonEqual";
+            buttonEqual.Size = new Size(180, 30);
+            buttonEqual.TabIndex = 14;
+            buttonEqual.Text = "=";
+            buttonEqual.UseVisualStyleBackColor = false;
+            buttonEqual.Click += buttonEqual_Click;
+            // 
+            // buttonC
+            // 
+            buttonC.BackColor = Color.LightCyan;
+            buttonC.Cursor = Cursors.Hand;
+            buttonC.FlatStyle = FlatStyle.Flat;
+            buttonC.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            buttonC.ForeColor = Color.MidnightBlue;
+            buttonC.Location = new Point(107, 121);
+            buttonC.Name = "buttonC";
+            buttonC.Size = new Size(40, 30);
+            buttonC.TabIndex = 13;
+            buttonC.Text = "C";
+            buttonC.UseVisualStyleBackColor = false;
+            buttonC.Click += buttonC_Click;
+            // 
+            // buttonDiv
+            // 
+            buttonDiv.BackColor = Color.LightSteelBlue;
+            buttonDiv.Cursor = Cursors.Hand;
+            buttonDiv.FlatStyle = FlatStyle.Flat;
+            buttonDiv.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            buttonDiv.ForeColor = Color.MidnightBlue;
+            buttonDiv.Location = new Point(153, 121);
+            buttonDiv.Name = "buttonDiv";
+            buttonDiv.Size = new Size(40, 30);
+            buttonDiv.TabIndex = 12;
+            buttonDiv.Text = "÷";
+            buttonDiv.UseVisualStyleBackColor = false;
+            buttonDiv.Click += Btn_MathOp_Click;
+            // 
+            // buttonSub
+            // 
+            buttonSub.BackColor = Color.LightSteelBlue;
+            buttonSub.Cursor = Cursors.Hand;
+            buttonSub.FlatStyle = FlatStyle.Flat;
+            buttonSub.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            buttonSub.ForeColor = Color.MidnightBlue;
+            buttonSub.Location = new Point(153, 49);
+            buttonSub.Name = "buttonSub";
+            buttonSub.Size = new Size(40, 30);
+            buttonSub.TabIndex = 11;
+            buttonSub.Text = "-";
+            buttonSub.UseVisualStyleBackColor = false;
+            buttonSub.Click += Btn_MathOp_Click;
+            // 
+            // buttonMul
+            // 
+            buttonMul.BackColor = Color.LightSteelBlue;
+            buttonMul.Cursor = Cursors.Hand;
+            buttonMul.FlatStyle = FlatStyle.Flat;
+            buttonMul.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            buttonMul.ForeColor = Color.MidnightBlue;
+            buttonMul.Location = new Point(153, 85);
+            buttonMul.Name = "buttonMul";
+            buttonMul.Size = new Size(40, 30);
+            buttonMul.TabIndex = 10;
+            buttonMul.Text = "×";
+            buttonMul.UseVisualStyleBackColor = false;
+            buttonMul.Click += Btn_MathOp_Click;
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.BackColor = Color.LightSteelBlue;
+            buttonAdd.Cursor = Cursors.Hand;
+            buttonAdd.FlatStyle = FlatStyle.Flat;
+            buttonAdd.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            buttonAdd.ForeColor = Color.MidnightBlue;
+            buttonAdd.Location = new Point(153, 13);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(40, 30);
+            buttonAdd.TabIndex = 11;
+            buttonAdd.Text = "+";
+            buttonAdd.UseVisualStyleBackColor = false;
+            buttonAdd.Click += Btn_MathOp_Click;
+            // 
+            // textDisplay2
+            // 
+            textDisplay2.BackColor = Color.SteelBlue;
+            textDisplay2.BorderStyle = BorderStyle.FixedSingle;
+            textDisplay2.Dock = DockStyle.Top;
+            textDisplay2.Font = new Font("Yu Gothic UI", 12F);
+            textDisplay2.ForeColor = Color.AliceBlue;
+            textDisplay2.Location = new Point(0, 0);
+            textDisplay2.Name = "textDisplay2";
+            textDisplay2.Size = new Size(205, 34);
+            textDisplay2.TabIndex = 12;
+            textDisplay2.Text = "0";
+            // 
+            // textDisplay1
+            // 
+            textDisplay1.BackColor = Color.SteelBlue;
+            textDisplay1.BorderStyle = BorderStyle.FixedSingle;
+            textDisplay1.Dock = DockStyle.Top;
+            textDisplay1.Font = new Font("Yu Gothic UI", 12F);
+            textDisplay1.ForeColor = Color.AliceBlue;
+            textDisplay1.Location = new Point(0, 34);
+            textDisplay1.Name = "textDisplay1";
+            textDisplay1.Size = new Size(205, 34);
+            textDisplay1.TabIndex = 13;
+            textDisplay1.Text = "0";
+            // 
+            // Form
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SteelBlue;
+            ClientSize = new Size(205, 269);
+            Controls.Add(textDisplay1);
+            Controls.Add(textDisplay2);
+            Controls.Add(panel1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form";
+            Text = "Calculator";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button0;
+        private Panel panel1;
+        private Button buttonDiv;
+        private Button buttonSub;
+        private Button buttonAdd;
+        private Button buttonMul;
+        private Button buttonC;
+        private Button buttonEqual;
+        private TextBox textDisplay2;
+        private TextBox textDisplay1;
     }
 }
--- Quick-Calc/Form1.cs
+++ Quick-Calc/Form1.cs
@@ -1,10 +1,88 @@
 namespace Quick_Calc
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        string operation = string.Empty;
+        string firstNum, secondNum;
+        bool enterValue = false;
+        int result = 0;
+
+        public Form()
         {
             InitializeComponent();
+        }
+
+        private void BtnNum_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Button button = (System.Windows.Forms.Button)sender;
+
+            if (textDisplay2.Text == "0" || enterValue)
+                textDisplay2.Text = "";
+
+            enterValue = false;
+            textDisplay2.Text += button.Text;
+        }
+
+        private void Btn_MathOp_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Button button = (System.Windows.Forms.Button)sender;
+
+            if (textDisplay2.Text == "") return;
+
+            if (result != 0)
+                buttonEqual.PerformClick();
+            else
+                result = int.Parse(textDisplay2.Text);
+
+            operation = button.Text;
+            enterValue = true;
+
+            textDisplay1.Text = result + " " + operation;
+        }
+
+        private void buttonEqual_Click(object sender, EventArgs e)
+        {
+            if (textDisplay2.Text == "" || operation == "") return;
+
+            secondNum = textDisplay2.Text;
+            textDisplay1.Text = $"{result} {operation} {secondNum} =";
+
+            int second = int.Parse(secondNum);
+
+            switch (operation)
+            {
+                case "+":
+                    result = result + second;
+                    break;
+
+                case "-":
+                    result = result - second;
+                    break;
+
+                case "×":
+                    result = result * second;
+                    break;
+
+                case "÷":
+                    if (second == 0)
+                    {
+                        MessageBox.Show("Cannot divide by zero");
+                        return;
+                    }
+                    result = result / second;
+                    break;
+            }
+
+            textDisplay2.Text = result.ToString();
+            operation = string.Empty;
+        }
+
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+            textDisplay2.Text = "0";
+            textDisplay1.Clear();
+            result = 0;
+            operation = string.Empty;
         }
     }
 }
